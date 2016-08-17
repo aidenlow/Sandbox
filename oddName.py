92a7142bcd96183f1a3f888df9d1cd12dc916bab
aidenlow <aiden.low@my.jcu.edu.au>
2016-08-18 09:42:22 +1000

    add name printing program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,14 @@
 """Aiden Low"""
+def main():
+    name = str(input("What is you first name"))
+    length_of_name = len(name)
+
+    while length_of_name < 1:
+        print("Invalid Name")
+        name = str(input("What is you first name"))
+        length_of_name = len(name)
+
+    sliced = name[::2]
+    print(sliced)
+
+main()
